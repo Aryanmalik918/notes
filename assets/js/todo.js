11a41a118dd2940aad347cc0169ab73586447c16
Aryanmalik918 <aryanmalik862@gmail.com>
2020-06-27 12:36:02 +0530

    initial commit

---
+++ assets/js/todo.js
@@ -0,0 +1,18 @@
+$("ul").on("click","li",function(){
+ $(this).toggleClass("line");	  
+});	     
+$("ul").on("click","span",function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove(); });
+	event.stopPropagation();
+});
+$("input[type=text]").keypress(function(event){
+	if(event.which === 13){
+    var toDo = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span><i class='fa fa-trash'></i> </span>"+ toDo+"</li>")
+    }
+});
+$(".fa-edit").click(function(){
+	$("input[type=text]").fadeToggle();
+});
